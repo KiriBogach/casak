afd8d1f3b967f75e7ef75efe33f72effa05b632e
Raspeberry <kyryl.bogach@gmail.com>
2020-04-08 15:27:00 +0200

    Initial Commit

---
+++ lang/es.php
@@ -0,0 +1,7 @@
+<?php
+$messages = array(
+    'title' => 'Galería de Casak',
+    'subgalleries' => 'Sub Galerías',
+    'keyboards_shortcuts' => '"Enter" para pantalla completa',
+    'copyright' => 'Galería de Casak 2019'
+);
